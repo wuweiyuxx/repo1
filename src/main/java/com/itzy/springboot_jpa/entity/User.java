01ede9404a977191ed473e27d8cd99f62af72a37
wuweiyuxx <zhangyuzzzyy@163.com>
2019-09-01 16:05:33 +0800

    tianjia

---
+++ src/main/java/com/itzy/springboot_jpa/entity/User.java
@@ -0,0 +1,21 @@
+package com.itzy.springboot_jpa.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String username;
+    private String password;
+    private String name;
+
+}
